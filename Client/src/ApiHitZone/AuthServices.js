2437ac8a39fe6fa38bf1d19ec31fd01e48b35688
Swyangdipta <swyangdipta20@gmail.com>
2024-03-21 22:56:15 +0530

    registration

---
+++ Client/src/ApiHitZone/AuthServices.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const postDataToApi = async (apiData) => {
+  try {
+    const apiUrl = "http://localhost:9000/users/signup";
+    console.log(apiData, "apiData");
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YourAuthToken', // Example: Bearer token
+      // Add any other headers you need
+    };
+    const response = await axios.post(apiUrl, apiData,{headers});
+
+    console.log("Response:", response.data);
+
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
--- Client/src/Registration/Registration.jsx
+++ Client/src/Registration/Registration.jsx
@@ -1,41 +1,116 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../CommonComponents/Navbar";
+import { postDataToApi } from "../ApiHitZone/AuthServices";
 
 const Registration = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const navigate = useNavigate();
-    const handleToggleRegistrationForm = () => {
-      navigate('/login')
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const navigate = useNavigate();
+  const handleToggleRegistrationForm = () => {
+    navigate("/login");
+  };
+  const submitForm = () => {
+    const payLoad = {
+      First_name: "shubho1431",
+      Last_name: "Acharyya",
+      Password: "subho12344",
+      Email: "abc44w1@gmail.com",
+      Phone: "9209976991",
+      User_type: "USER",
     };
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <div className='w-full max-w-md bg-white p-8 rounded-lg shadow-md'>
-        <div className="mb-4">
-              <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
-              <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-              <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="phone" className="block text-gray-700 font-bold mb-2">Phone Number</label>
-              <input type="text" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500" />
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-              <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500" />
-            </div>
-            <button onClick={()=>{}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">Register</button>
-            <p className="text-center mt-4">Already have an account? <button onClick={handleToggleRegistrationForm} className="text-blue-500 underline">Login</button></p>
-        </div>
-            
-          </div>
-  )
-}
+    postDataToApi(payLoad).then((res)=>{
+      console.log(res)
+    })
 
-export default Registration
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="phone"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Phone Number
+            </label>
+            <input
+              type="text"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <button
+            onClick={() => submitForm()}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          >
+            Register
+          </button>
+          <p className="text-center mt-4">
+            Already have an account?{" "}
+            <button
+              onClick={handleToggleRegistrationForm}
+              className="text-blue-500 underline"
+            >
+              Login
+            </button>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Registration;
